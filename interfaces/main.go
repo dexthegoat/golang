68b2df786d2a5168046c1d45ead25ae9ecf1d158
Jianshuo Yang <jyang63@stevens.edu>
2021-03-10 14:01:00 -0500

    meh

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,1 +1,36 @@
 package main
+
+import "fmt"
+
+type bot interface {
+	// 如果你有一个方法叫getGreeting()
+	// 而且返回了一个string 那你就是一种bot
+	getGreeting() string
+}
+
+type englishBot struct {
+}
+
+type spanishBot struct {
+}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	// some logic
+	return "Hi There!"
+}
+
+func (sb spanishBot) getGreeting() string {
+	return "Hola!"
+}
