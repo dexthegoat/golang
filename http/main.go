0dec81eabb8dcde40e126409a8942e91ecdf3ff6
Jianshuo Yang <jyang63@stevens.edu>
2021-03-11 08:28:30 -0500

    ended interfaces

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -12,5 +15,17 @@ func main() {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+
+	lw := logWriter{}
+	// bs := make([]byte, 9999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+	// io.Copy(os.Stdout, resp.Body)
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes", len(bs))
+	return len(bs), nil
 }
